83da584b9b11c95366f889ab7b82da861cbc0990
nicolas-jacquemin <nicolas1.jacquemin@epitech.eu>
2023-04-21 22:30:48 +0200

    [+] Add Swagger doc for API Documentation

--- src/router.ts
+++ src/router.ts
@@ -1,11 +1,24 @@
 import fs from "fs";
 import { Express } from "express";
+import swaggerUi from "swagger-ui-express";
+import * as yaml from "js-yaml";
 
 let routes: Array<string> = [];
 
 export default async function init_routes(app: Express): Promise<void> {
     console.log("Loading routes...");
     routes = [];
+    const swaggerFile: any = (process.cwd()+"/swagger/swagger.yaml");
+    const swaggerData: any = fs.readFileSync(swaggerFile, 'utf8');
+    const customCss: any = fs.readFileSync((process.cwd()+"/swagger/swagger.css"), 'utf8');
+    const swaggerDocument: any = yaml.load(swaggerData);
+    const options = {
+        customCss: customCss,
+        customSiteTitle: "Monkey Radio API",
+        customfavIcon: "https://monkeyradio.fr/assets/monkeyPNG.png"
+      };
+    app.use('/api/docs', swaggerUi.serve,
+            swaggerUi.setup(swaggerDocument, options, undefined, customCss));
     const get_files = async (fld: string) => {
         let dir = await fs.promises.readdir(process.env.ROUTE_SRC + fld);
         for (let dirO of dir) {
@@ -28,6 +41,9 @@ export default async function init_routes(app: Express): Promise<void> {
             app.use(`/${route}`, module.default);
         numCalls++;
         if (numCalls === routes.length) {
+            app.use((req, res) => {
+                res.status(404).json({ message: "Route not found, please visit /api/docs" });
+            });
             console.log("Routes loaded.");
             return;
         }
