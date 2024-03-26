93a8bde883df897d5ff5330a14100cda9fd5e669
Nicolas Jacquemin <jacquemin.nicolas59@gmail.com>
2024-03-26 22:12:12 +0100

    feat - add origin on pr netlify preview

--- src/main.ts
+++ src/main.ts
@@ -13,7 +13,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: [/localhost(:\d*)?$/, /.monkeyradio.fr$/],
+    origin: [/localhost(:\d*)?$/, /.monkeyradio.fr$/, /.monkeyradio.netlify.app$/],
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
